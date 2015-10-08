245d09e944f80a969191123d4332e9fbb6c2bb9f
MarcosInstale <marcos.murilo@instaletecnologia.com.br>
2015-10-08 14:40:44 -0300

    Estrutura Inicial

---
+++ CicloVidaMVC.Test/Usuario.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTestCicloVida
+
+    <TestMethod()> Public Sub TestMethodCadUser()
+    End Sub
+
+End Class
